b219a745fc04452a212e880860b400f80cb31346
pkyujin <luxoris02@sookmyung.ac.kr>
2026-07-16 00:37:34 +0900

    feat: 금융 상품 화면 UI 구현

--- app/src/main/java/com/umc/homefit/navigation/NavGraph.kt
+++ app/src/main/java/com/umc/homefit/navigation/NavGraph.kt
@@ -156,7 +156,7 @@ fun MainScreen(
         currentDestination?.route?.contains(TabRoute.Home::class.qualifiedName.orEmpty()) == true -> "홈"
         currentDestination?.route?.contains(TabRoute.RecruitmentList::class.qualifiedName.orEmpty()) == true -> "공고 조회"
         currentDestination?.route?.contains(TabRoute.Analysis::class.qualifiedName.orEmpty()) == true -> "입주 분석"
-        currentDestination?.route?.contains(TabRoute.Finance::class.qualifiedName.orEmpty()) == true -> "금융 상품 추천"
+        currentDestination?.route?.contains(TabRoute.Finance::class.qualifiedName.orEmpty()) == true -> "금융 상품"
         currentDestination?.route?.contains(TabRoute.MyPage::class.qualifiedName.orEmpty()) == true -> "마이페이지"
         else -> "HomeFit"
     }
