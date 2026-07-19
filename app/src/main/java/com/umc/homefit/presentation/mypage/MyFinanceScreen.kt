2463072cc2464785454d5ad77630c7ba480f641b
seoyoung <tjdud51238@gmail.com>
2026-07-19 18:26:17 +0900

    style: 사용하지 않는 import 정리

--- app/src/main/java/com/umc/homefit/presentation/mypage/MyFinanceScreen.kt
+++ app/src/main/java/com/umc/homefit/presentation/mypage/MyFinanceScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -27,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
