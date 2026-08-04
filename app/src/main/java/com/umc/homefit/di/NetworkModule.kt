911777261d5b1cb853802d31b8cd5e5ff7e18c68
9nuoyoes <tjdud51238@gmail.com>
2026-08-04 20:30:49 +0900

    feat: 마이페이지 프로필 API 연동 (#61)

--- app/src/main/java/com/umc/homefit/di/NetworkModule.kt
+++ app/src/main/java/com/umc/homefit/di/NetworkModule.kt
@@ -1,6 +1,5 @@
 package com.umc.homefit.di
 
-import com.umc.homefit.data.api.analysis.AnalysisApiService
 import com.umc.homefit.data.api.auth.AuthApiService
 import com.umc.homefit.data.api.common.HealthApiService
 import com.umc.homefit.data.api.finance.FinanceApiService
@@ -17,6 +16,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+import com.umc.homefit.data.api.mypage.MyPageApiService
+import com.umc.homefit.data.api.analysis.AnalysisApiService
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -85,4 +87,14 @@ object NetworkModule {
     @Singleton
     fun provideAuthApiService(retrofit: Retrofit): AuthApiService =
         retrofit.create(AuthApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideAnalysisApiService(retrofit: Retrofit): AnalysisApiService =
+        retrofit.create(AnalysisApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideMyPageApiService(retrofit: Retrofit): MyPageApiService =
+        retrofit.create(MyPageApiService::class.java)
 }
--- app/src/main/java/com/umc/homefit/presentation/mypage/MyPageScreen.kt
+++ app/src/main/java/com/umc/homefit/presentation/mypage/MyPageScreen.kt
@@ -21,7 +21,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -44,7 +44,7 @@ fun MyPageScreenRoute(
     viewModel: MyPageScreenViewModel,
     onNavigateToSaved: () -> Unit, onNavigateToNotification: () -> Unit, onNavigateToFinance: () -> Unit, modifier: Modifier = Modifier
 ) {
-    val uiState by viewModel.uiState.collectAsState()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     MyPageScreen(
         uiState = uiState,
         onNavigateToSaved = onNavigateToSaved,
