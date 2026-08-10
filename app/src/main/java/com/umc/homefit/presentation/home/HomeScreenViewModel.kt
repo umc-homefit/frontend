a477096fdd90ac968cf288db9340503cb3028859
pkyujin <128917326+pkyujin@users.noreply.github.com>
2026-08-10 21:42:51 +0900

    feat: 금융상품 상세 조회 api 연동 (#83)

--- app/src/main/java/com/umc/homefit/presentation/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/umc/homefit/presentation/home/HomeScreenViewModel.kt
@@ -2,12 +2,9 @@
 
 
 import androidx.lifecycle.ViewModel
-import com.umc.homefit.data.dto.recruitment.RecruitmentDto
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
